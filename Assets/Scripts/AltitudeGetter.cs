44fcb276191919c76cb8b9f6035b9312e21fc6b1
Julian Korevaar <519652@student.glu.nl>
2019-09-10 14:39:25 +0200

    Mapbox package update
    over 2000 changes
    Good Luck Have Fun

---
+++ Assets/Scripts/AltitudeGetter.cs
@@ -0,0 +1,40 @@
+﻿using Mapbox.Map;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Data;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+using UnityEngine;
+
+public class AltitudeGetter : MonoBehaviour
+{
+    [SerializeField]
+    private AbstractMap map;
+
+    public static AltitudeGetter altitudeGetter;
+
+    void spawnPrefabWithHeight(double lat, double lon)
+    {
+        //get tile ID
+        UnwrappedTileId tileIDUnwrapped = TileCover.CoordinateToTileId(new Mapbox.Utils.Vector2d(lat, lon), (int)map.Zoom);
+
+        //get tile
+        UnityTile tile = map.MapVisualizer.GetUnityTileFromUnwrappedTileId(tileIDUnwrapped);
+
+        //lat lon to meters because the tiles rect is also in meters
+        Vector2d v2d = Conversions.LatLonToMeters(new Mapbox.Utils.Vector2d(lat, lon));
+        //get the origin of the tile in meters
+        Vector2d v2dcenter = tile.Rect.Center - new Mapbox.Utils.Vector2d(tile.Rect.Size.x / 2, tile.Rect.Size.y / 2);
+        //offset between the tile origin and the lat lon point
+        Vector2d diff = v2d - v2dcenter;
+
+        //maping the diffetences to (0-1)
+        float Dx = (float)(diff.x / tile.Rect.Size.x);
+        float Dy = (float)(diff.y / tile.Rect.Size.y);
+
+        //height in unity units
+        float h = tile.QueryHeightData(Dx, Dy);
+
+        Debug.Log(h);
+        //Vector3 location = Conversions.GeoToWorldPosition(lat, lon, _map.CenterMercator, _map.WorldRelativeScale).ToVector3xz();
+    }
+}
