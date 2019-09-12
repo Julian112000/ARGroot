9fec15515cce6d9bc34f59699289553a6445b719
Max Dekker <xamrekked@gmail.com>
2019-09-12 13:10:29 +0200

    Select Model UI Update
    Complete UI

---
+++ Assets/Scripts/UI/OpenTab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTab : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject modelTab;
+    [SerializeField]
+    private GameObject arrow, arrowDown;
+
+    public void OpenBuildTab()
+    {
+        Debug.Log("Opened tab");
+        if (modelTab.activeSelf)
+        {
+            modelTab.SetActive(false);
+            arrow.SetActive(true);
+            arrowDown.SetActive(false);
+        }
+        else
+        {
+            modelTab.SetActive(true);
+            arrow.SetActive(false);
+            arrowDown.SetActive(true);
+        }
+
+    }
+}
